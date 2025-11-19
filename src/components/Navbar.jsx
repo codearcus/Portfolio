e98b87fd78a29644b78a878eaacefffe2fe0d316
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-11-19 10:43:00 +0530

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,102 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+        <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <Image
+              src={"https://flowbite.com/docs/images/logo.svg"}
+              alt="FLowbite"
+              width={20}
+              height={20}
+            />
+            <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              type="button"
+              className=" bg-blue-200 hover:bg-blue-500 cursor-pointer rounded-2xl box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none"
+            >
+              Get started
+            </button>
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  d="M5 7h14M5 12h14M5 17h14"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-default rounded-base bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary ">
+              <li className="hover:scale-110 hover:transition-transform duration-300">
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-heading bg-brand rounded-sm md:bg-transparent md:text-fg-brand md:p-0"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="hover:scale-110 hover:transition-transform duration-300">
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  About
+                </a>
+              </li>
+              <li className="hover:scale-110 hover:transition-transform duration-300">
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Services
+                </a>
+              </li>
+              <li className="hover:scale-110 hover:transition-transform duration-300">
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
